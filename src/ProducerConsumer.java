aa3785781e0701dd75c5282cc8726b083aeca4fd
Alwin Sunu <alwinsunu1974@gmail.com>
2026-02-21 18:22:35 +0530

    Refactor ProducerConsumer class for synchronization

--- src/ProducerConsumer.java
+++ src/ProducerConsumer.java
@@ -1,72 +1,73 @@
-class SharedResource {
     int item;
     boolean available = false;
-    synchronized void put(int item) {
-        try {
-            while (available) {
-                wait(); 
+
+    // TODO: synchronize void put(int item)
+    synchronized void put(int item){
+        while(available){
+           try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-            this.item = item;
-            available = true;
-            System.out.println("Produced: " + item);
-            notify(); 
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
+        this.item=item;
+        available=true;
+        System.out.println("Produced: "+item);
+        notify();
     }
-    synchronized void get() {
-        try {
-            while (!available) {
-                wait(); 
+    
+    synchronized void get(){
+        while(!available){
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-            System.out.println("Consumed: " + item);
-            available = false;
-            notify(); // Notify producer
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
+        System.out.println("Consumed: "+item);
+        available=false;
+        notify();
     }
 }
 
 class Producer extends Thread {
+
     SharedResource resource;
-
-    Producer(SharedResource resource) {
-        this.resource = resource;
+    Producer(SharedResource obj) {
+        this.resource = obj;
     }
-
-    public void run() {
-        for (int i = 1; i <= 5; i++) {
+    // TODO: Constructor to init resource
+    
+    public void run(){
+        for (int i =1; i <=5; i++) {
             resource.put(i);
         }
     }
+
 }
 
 class Consumer extends Thread {
+
     SharedResource resource;
-
-    Consumer(SharedResource resource) {
-        this.resource = resource;
+    Consumer(SharedResource obj) {
+        this.resource = obj;
     }
-
-    public void run() {
-        for (int i = 1; i <= 5; i++) {
+    // TODO: Constructor to init resource
+    public void run(){
+        for (int i =1; i <=5; i++) {
             resource.get();
         }
     }
+
 }
 
 public class ProducerConsumer {
     public static void main(String[] args) {
-        SharedResource resource = new SharedResource();
-        Producer p = new Producer(resource);
-        Consumer c = new Consumer(resource);
-        p.start();
-        c.start();
-    }
-    }    public static void main(String[] args) {
         // TODO: Create SharedResource object
-        // TODO: Create Producer and Consumer threads
-        // TODO: Start both threads
+        SharedResource obj=new SharedResource();
+        Producer t1=new Producer(obj);
+        Consumer t2=new Consumer(obj);
+        t1.start();
+        t2.start();
     }
 }
